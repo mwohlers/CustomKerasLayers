f909d2d810fd69746c4f36fa615ba28ef795186c
Mark Wohlers <hramww@PFR.CO.NZ>
2020-01-15 16:55:29 +1300

    added setup.py and moved sturcture up one level

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CustomKerasLayers',
+    version='0.1',
+    packages=['CustomKerasLayers', 'CustomKerasLayers.layers', 'CustomKerasLayers.models'],
+    url='https://github.com/mwohlers/CustomKerasLayers',
+    license='',
+    author='Zelgunn',
+    author_email='',
+    description='forked from Zelgunn/CustomKerasLayers'
+)
